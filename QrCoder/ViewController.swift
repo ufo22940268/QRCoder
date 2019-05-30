633c027d1dd7b371d72eb5f670775230c24fd222
Frank Cheng <ufo22940268@gmail.com>
2019-05-30 09:50:46 +0800

    Initial Commit

---
+++ QrCoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QrCoder
+//
+//  Created by Frank Cheng on 2019/5/30.
+//  Copyright © 2019 Frank Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
