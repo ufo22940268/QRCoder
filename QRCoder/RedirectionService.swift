dbea656ad05170eae483f3e1a8e8db82a7769343
Frank Cheng <ufo22940268@gmail.com>
2019-06-16 09:10:49 +0800

    v

---
+++ QRCoder/RedirectionService.swift
@@ -0,0 +1,14 @@
+//
+//  RedirectionService.swift
+//  QRCoder
+//
+//  Created by Frank Cheng on 2019/6/15.
+//  Copyright © 2019 Frank Cheng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RedirectionService {
+    static let shared = RedirectionService()
+}
